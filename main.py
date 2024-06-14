ecfd9f4ec526655e65eb274dbd022b3fe839d306
Roshan310 <yuvrajaryal83@gmail.com>
2024-06-14 20:52:37 +0545

    multiple operation can be done
    expression like '3+4+5-6' or '4*5*6*3' is now possible. It still can't
    perform multiple operations on same line like '3+4*6/4'. It is a work in
    progress

--- main.py
+++ main.py
@@ -82,36 +82,32 @@ class Interpreter:
         else:
             self.error()
 
+    def term(self):
+        """Return the actual value of the INTEGER token"""
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
+
     def expr(self):
         self.current_token = self.get_next_token()
 
-        left = self.current_token
-        self.eat(INTEGER)
-
-        op = self.current_token
-        if op.type == PLUS:
-            self.eat(PLUS)
-        elif op.type == MINUS:
-            self.eat(MINUS)
-        elif op.type == DIVIDE:
-            self.eat(DIVIDE)
-        elif op.type == MULTIPLY:
-            self.eat(MULTIPLY)
-
-        right = self.current_token
-        self.eat(INTEGER)
-
-        if op.type == PLUS:
-            result = left.value + right.value
-        elif op.type == MINUS:
-            result = left.value - right.value
-        elif op.type == DIVIDE:
-            result = left.value / right.value
-        elif op.type == MULTIPLY:
-            result = left.value * right.value
+        result = self.term()
+        while self.current_token.type in (PLUS, MINUS, DIVIDE, MULTIPLY):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result = result + self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result = result - self.term()
+            elif token.type == DIVIDE:
+                self.eat(DIVIDE)
+                result = result/self.term()
+            elif token.type == MULTIPLY:
+                self.eat(MULTIPLY)
+                result = result * self.term()
 
         return result
-
 
 def main():
     while True:
