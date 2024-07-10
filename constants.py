f53a1b7db9e901a41afcd41a8f5db698324e7ebd
Roshan310 <yuvrajaryal83@gmail.com>
2024-07-10 21:12:19 +0545

    move all the constant to new file

---
+++ constants.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TokenType(Enum):
+    
+    INTEGER = "INTEGER"
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    MULTIPLY = "MULTIPLY"
+    LPAREN = "LPAREN"
+    RPAREN = "RPAREN"
+    BEGIN = "BEGIN"
+    END = "END"
+    ID = "ID"
+    DOT = "DOT"
+    SEMI = "SEMI"
+    ASSIGN = "ASSIGN"
+    COLON = "COLON"
+    PROGRAM = "PROGRAM"
+    VAR = "VAR"
+    COMMA = "COMMA"
+    INTEGER_CONST = "INTEGER_CONST"
+    REAL_CONST = "REAL_CONST"
+    INTEGER_DIV = "INTEGER_DIV"
+    FLOAT_DIV = "FLOAT_DIV"
+    REAL = "REAL"
+    EOF = "EOF"
+
